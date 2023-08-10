55f62f6ee69008b691eff27576314cf8ee741bc4
LuisC92 <luis.clara@wildcodeschool.com>
2023-08-10 10:19:24 +0100

    fix small issue on import

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -14,6 +14,7 @@ function BookList() {
   };
 
   const getBook = () => {
+    console.log(searchBook)
     axios
       .get(`http://openlibrary.org/search.json?title=${searchBook}`)
       .then((response) => {
